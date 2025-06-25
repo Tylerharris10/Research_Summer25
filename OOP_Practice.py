5c5204ec8f2203e9b2b906ac55f99650d3c0fabe
Tyler Harris <th7678@princeton.edu>
2025-06-25 17:58:06 -0400

    Adding OOP Prac2

--- OOP_Practice.py
+++ OOP_Practice.py
@@ -25,3 +25,36 @@ p1.liscenseInfo()
 del p1.age
 p1.age = 40
 print(p1.age)
+
+class Car:
+
+    def __init__(self, brand, model, numSeats, currentSpeed):
+        self.brand = brand
+        self.model = model
+        self.numSeats = numSeats
+        self.currentSpeed = currentSpeed
+    
+    def __str__(self):
+        return f"{self.brand}({self.model})({self.numSeats})({self.currentSpeed})"
+    
+    def speeding(self):
+        if self.currentSpeed > 65:
+            return True
+        else:
+            return False
+    def isSporty(self):
+        if self.numSeats == 2:
+            return True
+        else:
+            return False
+
+porsche = Car('Porsche', 'Boxster', 2, 200)
+print(porsche)
+print(porsche.speeding())
+print(porsche.isSporty())
+del porsche.currentSpeed
+porsche.currentSpeed = 40
+print(porsche.speeding())
+del porsche.numSeats
+porsche.numSeats = 4
+print(porsche.isSporty())
